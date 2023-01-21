d676a85b420cb41bd9b7ec1bb5ddb6820d3530ae
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-01-21 13:54:59 -0300

    refacts GenreicButton comp

--- src/components/GenericButton.tsx
+++ src/components/GenericButton.tsx
@@ -1,11 +1,17 @@
-const GenericButton = (props: {
+interface GenericButtonProps {
   children: string;
   onClick: () => void;
   addedClasses?: string;
-}) => {
+}
+
+const GenericButton = (props: GenericButtonProps): JSX.Element => {
   const { children, onClick, addedClasses } = props;
+  let classesToAdd = '';
+
+  if (addedClasses) classesToAdd = ' ' + addedClasses;
+
   return (
-    <button className={`genericButton ${addedClasses}`} onClick={onClick}>
+    <button className={`genericButton${classesToAdd}`} onClick={onClick}>
       {children}
     </button>
   );
