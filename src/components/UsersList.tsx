1b45545af4c60a7b7153503bd4b4822f36dc09af
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-01-21 13:55:51 -0300

    creates barrel file & refacts UsersList comp

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react';
-import UserCard from './UserCard';
-import GenericButton from './GenericButton';
-import { UsersQty } from '../types';
+import { useState, useEffect } from 'react';
+import { UserCard, GenericButton } from './index';
+import fetchData from '../services/fetchData';
+import { User, APIUser, UsersQty } from '../interfaces';
+import useCounter from '../hooks/useCounter';
+import mapFromApiToUser from '../utils/usersTransform';
 
-const UsersList = () => {
-  const [count, setCount] = useState<UsersQty>(1);
-
-  const handleClick = (n: number) => {
-    let result = count + n;
-
-    if (result !== 6 && result !== 0) {
-      setCount(result as UsersQty);
-      return;
-    }
+interface UsersListState {
+  users: Array<User>;
+  usersAmount: {
+    amount: UsersQty;
   };
+}
+
+interface UsersListProps {
+  handleError: (errorMsg: string) => void;
+}
+
+const UsersList = (props: UsersListProps): JSX.Element => {
+  const { handleError } = props;
+  const [usersAmount, setUsersAmount] = useState<UsersListState['usersAmount']>(
+    { amount: 5 }
+  );
+  const [users, setUsers] = useState<UsersListState['users']>([]);
+  const [count, handleAddition] = useCounter(5);
+
+  useEffect(() => {
+    fetchData.getUsers(usersAmount.amount).then(APIResponse => {
+      if (APIResponse.isOk) {
+        setUsers(mapFromApiToUser(APIResponse.data as Array<APIUser>));
+      } else {
+        handleError(APIResponse.data as string);
+      }
+    });
+  }, [usersAmount]);
 
   return (
     <div className='users'>
       <h1 className='users__title'>Users</h1>
       <div className='users__list'>
-        <UserCard />
-        <UserCard />
-        <UserCard />
+        {users.map(
+          (user): JSX.Element => (
+            <UserCard key={user.id} user={user} />
+          )
+        )}
       </div>
       <div className='users__keypad'>
         <p>{count}</p>
-        <GenericButton onClick={() => handleClick(1)}>Sumar</GenericButton>
-        <GenericButton onClick={() => handleClick(-1)}>Restar</GenericButton>
-        <GenericButton onClick={() => setCount(1)}>Buscar</GenericButton>
+        <GenericButton onClick={() => handleAddition(1)}>+</GenericButton>
+        <GenericButton onClick={() => handleAddition(-1)}>-</GenericButton>
+        <GenericButton onClick={() => setUsersAmount({ amount: count })}>
+          Refresh
+        </GenericButton>
       </div>
     </div>
   );
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as GenericButton } from './GenericButton';
+export { default as FavList } from './FavList';
+export { default as UsersList } from './UsersList';
+export { default as UserCard } from './UserCard';
