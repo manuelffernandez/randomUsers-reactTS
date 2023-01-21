9f5da57c419150e8b27f22abc9fee811dbb045bf
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-01-21 13:56:03 -0300

    Refacts App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { useEffect } from 'react';
-import fetchData from './services/fetchData';
-import UsersList from './components/UsersList';
-import FavList from './components/FavList';
+import { useState } from 'react';
+import { UsersList, FavList } from './components';
+
+interface AppState {
+  error: { isError: boolean; errorMsg?: string };
+}
 
 function App() {
-  useEffect(() => {
-    // fetchData.getUsers(5);
-  }, []);
+  const [error, setError] = useState<AppState['error']>({ isError: false });
 
-  return (
-    <div className='App'>
-      <UsersList></UsersList>
-      <FavList></FavList>
-    </div>
-  );
+  const handleError = (errorMsg: string): void => {
+    setError({ isError: true, errorMsg });
+  };
+
+  const loadContent = () => {
+    if (!error.isError) {
+      return (
+        <>
+          <UsersList handleError={handleError}></UsersList>
+          <FavList />
+        </>
+      );
+    }
+    return <h1>Error {error.errorMsg}</h1>;
+  };
+
+  return <div className='App'>{loadContent()}</div>;
 }
 
 export default App;
